b434c85d63939c8a47d3b9a69091115870f6803e
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-09-09 12:00:45 +0300

    emit remarks

--- opencl-execute.sh
+++ opencl-execute.sh
@@ -1,43 +1,46 @@
+# Execute all OpenCL kernels and save emitted remarks to file where each kernel
+# reside.
+rm -rf $POCL_CACHE_DIR
 cwd=$(pwd)
 echo "covariance"
-cd ${cwd}/OpenCL/datamining/covariance/ && ./covariance.exe
+cd ${cwd}/OpenCL/datamining/covariance/ && ./covariance.exe | grep 'unknown' > remarks
 echo "correlation"
-cd ${cwd}/OpenCL/datamining/correlation && ./correlation.exe
+cd ${cwd}/OpenCL/datamining/correlation && ./correlation.exe | grep 'unknown' > remarks
 echo "jacobi2D"
-cd ${cwd}/OpenCL/stencils/jacobi-2d-imper && ./jacobi2D.exe
+cd ${cwd}/OpenCL/stencils/jacobi-2d-imper && ./jacobi2D.exe | grep 'unknown' > remarks
 echo "jacobi1D"
-cd ${cwd}/OpenCL/stencils/jacobi-1d-imper && ./jacobi1D.exe
+cd ${cwd}/OpenCL/stencils/jacobi-1d-imper && ./jacobi1D.exe | grep 'unknown' > remarks
 echo "adi"
-cd ${cwd}/OpenCL/stencils/adi && ./adi.exe
+cd ${cwd}/OpenCL/stencils/adi && ./adi.exe | grep 'unknown' > remarks
 echo "3DConvolution"
-cd ${cwd}/OpenCL/stencils/convolution-3d && ./3DConvolution.exe
+cd ${cwd}/OpenCL/stencils/convolution-3d && ./3DConvolution.exe | grep 'unknown' > remarks
 echo "fdtd2d"
-cd ${cwd}/OpenCL/stencils/fdtd-2d && ./fdtd2d.exe
+cd ${cwd}/OpenCL/stencils/fdtd-2d && ./fdtd2d.exe | grep 'unknown' > remarks
 echo "2DConvolution"
-cd ${cwd}/OpenCL/stencils/convolution-2d && ./2DConvolution.exe
+cd ${cwd}/OpenCL/stencils/convolution-2d && ./2DConvolution.exe | grep 'unknown' > remarks
 echo "mvt"
-cd ${cwd}/OpenCL/linear-algebra/kernels/mvt && ./mvt.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/mvt && ./mvt.exe | grep 'unknown' > remarks
 echo "atax"
-cd ${cwd}/OpenCL/linear-algebra/kernels/atax && ./atax.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/atax && ./atax.exe | grep 'unknown' > remarks
 echo "gesummv"
-cd ${cwd}/OpenCL/linear-algebra/kernels/gesummv && ./gesummv.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/gesummv && ./gesummv.exe | grep 'unknown' > remarks
 echo "2mm"
-cd ${cwd}/OpenCL/linear-algebra/kernels/2mm && ./2mm.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/2mm && ./2mm.exe | grep 'unknown' > remarks
 echo "3mm"
-cd ${cwd}/OpenCL/linear-algebra/kernels/3mm && ./3mm.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/3mm && ./3mm.exe | grep 'unknown' > remarks
 echo "syr2k"
-cd ${cwd}/OpenCL/linear-algebra/kernels/syr2k && ./syr2k.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/syr2k && ./syr2k.exe | grep 'unknown' > remarks
 echo "bicg"
-cd ${cwd}/OpenCL/linear-algebra/kernels/bicg && ./bicg.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/bicg && ./bicg.exe | grep 'unknown' > remarks
 echo "gemm"
-cd ${cwd}/OpenCL/linear-algebra/kernels/gemm && ./gemm.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/gemm && ./gemm.exe | grep 'unknown' > remarks
 echo "gemver"
-cd ${cwd}/OpenCL/linear-algebra/kernels/gemver && ./gemver.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/gemver && ./gemver.exe | grep 'unknown' > remarks
 echo "syrk"
-cd ${cwd}/OpenCL/linear-algebra/kernels/syrk && ./syrk.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/syrk && ./syrk.exe | grep 'unknown' > remarks
 echo "doitgen"
-cd ${cwd}/OpenCL/linear-algebra/kernels/doitgen && ./doitgen.exe
+cd ${cwd}/OpenCL/linear-algebra/kernels/doitgen && ./doitgen.exe | grep 'unknown' > remarks
 echo "gramschmidt"
-cd ${cwd}/OpenCL/linear-algebra/solvers/gramschmid && ./gramschmidt.exe
+cd ${cwd}/OpenCL/linear-algebra/solvers/gramschmidt && ./gramschmidt.exe | grep 'unknown' > remarks
 echo "lu"
-cd ${cwd}/OpenCL/linear-algebra/solvers/lu && ./lu.exe
+cd ${cwd}/OpenCL/linear-algebra/solvers/lu && ./lu.exe | grep 'unknown' > remarks
